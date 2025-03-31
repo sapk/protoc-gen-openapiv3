2ab986f483dd80632c6da2cf44283f3fbe144988
Antoine GIRARD <antoine.girard@sapk.fr>
2025-04-01 01:33:57 +0200

    order yaml files output

--- testdata/generate.sh
+++ testdata/generate.sh
@@ -4,3 +4,8 @@
 
 protoc --plugin=protoc-gen-openapiv3=../protoc-gen-openapiv3 --openapiv3_out=output=./test.openapi.yaml,output-format=yaml:. --proto_path=./ ./test.proto
 protoc --plugin=protoc-gen-openapiv3=../protoc-gen-openapiv3 --openapiv3_out=output=./test.v2.openapi.yaml,output-format=yaml:. --proto_path=./ ./test.v2.proto
+
+# Sort and format YAML files consistently
+# go install github.com/mikefarah/yq/v4@latest
+yq eval 'sortKeys(..)' -i ./test.openapi.yaml
+yq eval 'sortKeys(..)' -i ./test.v2.openapi.yaml
